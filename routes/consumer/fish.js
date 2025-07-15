a9fa1a0f4f217330e99287390d65980ed08a87aa
Muhammad Razin Syakib <muhammadrazin14@gmail.com>
2025-07-16 04:20:11 +0700

    ILIKE

--- routes/consumer/fish.js
+++ routes/consumer/fish.js
@@ -114,7 +114,7 @@ router.get("/cari/", async (req, res) => {
             JOIN seller s
             ON f.id_seller = s.id
             WHERE f.name
-            LIKE $1`;
+            ILIKE $1`;
 
             const result = await client.query(query, [`%${fishName}%`]);
             if (result.rows.length === 0) {
